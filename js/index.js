deb1e716f695833b02de39ad84ee23e1cd9844d0
AndresRamirez917 <andresmintic2021@gmail.com>
2024-10-02 12:43:43 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -10,18 +10,75 @@ for(let i of dessertsArr.slice(0, 1)){
             
             <div class="card card-1">
                 <img src="${element.strMealThumb}" alt="">
-                <h3>Jhoanna Doe</h3>
-                <h4>Web Developer</h4>
+                <h3>${element.strMeal}</h3>
+                <h4>${element.strCategory}</h4>
             </div>
 
             `)
             const speakers_row = document.querySelector('.speakers-row');
-            speakers_row.append(card)
-
-}
-          
-        
+            speakers_row.append(card);
+}     
     });
 }
 
+function events(){
+    for(i = 1; i < 5; i++){
+        const box = document.createRange().createContextualFragment(`
+            
+                <div class="box box-${i}">
+                   <h3>27 October</h3>
+                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, voluptates.</p>
+                   <button class="btn button-2">more details</button>
+                </div>
+            
+            `)
+            const events_row = document.querySelector('.events-row');
+            events_row.append(box)
+    }
+}
+
+
+
+const btn_enviar = document.getElementById('btn-enviar');
+const enviar = (e) => {
+e.preventDefault();
+const nonmbre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const mensaje = document.getElementById('mensaje');
+const arr = [];
+const messageArr = ["Nombre", "Email", "Mensaje"];
+arr.push(nonmbre, email, mensaje);
+for(i = 0; i < arr.length; i++){
+    if(arr[i].value == ""){
+        swal({
+            title: `El campo ${messageArr[i]} no puede estar vacío`,
+            icon: "error",
+             })
+             return false;
+    }
+}
+if(!emailValido(email)){
+    swal({
+        title: `El campo ${messageArr[1]} no tiene el formato correcto`,
+        icon: "error",
+         })
+         return false;
+}
+swal({
+    title: `Datos enviados satisfactoriamente`,
+    icon: "success",
+     })
+     nonmbre.value = "";
+     email.value = "";
+     mensaje.value = "";
+return true;
+}
+
+const emailValido = (email) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)
+}
+
+btn_enviar.addEventListener("click", enviar)
+
 getData()
+events()
